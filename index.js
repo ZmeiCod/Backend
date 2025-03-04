aa389384eeddcdf610bd3b75ce25788284718288
NoNameCoder <johnthetheologian@mail.ru>
2025-03-04 15:28:01 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -6,24 +6,39 @@ const models = require("./models/models");
 
 const cors = require("cors");
 const router = require("./routes/index");
-const frontpadRouter = require('./routes/frontpadRouter');
+const frontpadRouter = require("./routes/frontpadRouter");
 const errorHandler = require("./middleware/ErrorHandlingMiddleware");
 const fileUpload = require("express-fileupload");
 const path = require("path");
 
 const PORT = process.env.PORT;
+
+const allowedOrigins = [
+  "http://localhost:3000",
+  "http://81.177.223.206:7878",
+  "http://81.177.223.206",
+];
 let corsOptions = {
-  origin: ['http://localhost:3000'],
-}
+  origin: (origin, callback) => {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      console.log("Origin not allowed by CORS:", origin);
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  optionsSuccessStatus: 200,
+  credentials: true,
+};
 
 const app = express();
 app.use(cors(corsOptions));
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.resolve(__dirname, "static")));
 app.use(fileUpload({}));
 app.use("/api", router);
-app.use('/api/frontpad', frontpadRouter);
+app.use("/api/frontpad", frontpadRouter);
 app.use(errorHandler);
 
 const start = async () => {
